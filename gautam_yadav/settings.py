688ce12afcfbb54f6dc019cd7620abb14e4b6f4d
gautam.yadav <gautam.yadav@walsonslabs.com>
2026-05-09 23:52:37 +0530

    fix django admin static files

--- gautam_yadav/settings.py
+++ gautam_yadav/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,13 +125,15 @@ USE_TZ = True
 
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
