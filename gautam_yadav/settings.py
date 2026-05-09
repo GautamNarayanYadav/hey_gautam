ee611e56a2022f268bc3a6f8efd4e8bbfaf7a4b9
gautam.yadav <gautam.yadav@walsonslabs.com>
2026-05-09 23:11:22 +0530

    add render env config

--- gautam_yadav/settings.py
+++ gautam_yadav/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-logdbi0g)!kvn#_*=$dtfwy4f=mrxg!5&$cic06r7fzs6=g#ub'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = []
 
 
