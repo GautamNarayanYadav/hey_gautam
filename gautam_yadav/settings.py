c758a4abd4b99be19af5b24efd7fe3d47d92ab82
gautam.yadav <gautam.yadav@walsonslabs.com>
2026-05-09 22:57:20 +0530

    add static root

--- gautam_yadav/settings.py
+++ gautam_yadav/settings.py
@@ -122,9 +122,12 @@ USE_TZ = True
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
