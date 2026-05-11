649a7fb75cfab7e12f39e6510a761967ea53ef7b
gautam.yadav <gautam.yadav@walsonslabs.com>
2026-05-11 16:45:52 +0530

    also migrations

---
+++ gautam_yadav/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+
+def home(request):
+
+    return render(
+        request,
+        'index.html'
+    )
+
+
+def contact(request):
+
+    return render(
+        request,
+        'contact.html'
+    )
+
+
+def about(request):
+
+    return render(
+        request,
+        'about.html'
+    )
+
+
+def blog(request):
+
+    return render(
+        request,
+        'blogs.html'
+    )
